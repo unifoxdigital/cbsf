5ab180a722388a1a0b3788b32e60e97f2b73baaf
hihasan <hasan@hihasan.com>
2018-11-07 13:08:04 +0600

    files

---
+++ header.php
@@ -0,0 +1,75 @@
+<header id="header" data-plugin-options='{"stickyEnabled": true, "stickyEnableOnBoxed": true, "stickyEnableOnMobile": true, "stickyStartAt": 57, "stickySetTop": "-47px", "stickyChangeLogo": true}'>
+				<div class="header-body">
+					<div class="header-container container">
+						<div class="header-row">
+							<div class="header-column">
+								<div class="header-logo">
+									<a href="index.php">
+										<img alt="gb" width="80" height="60" data-sticky-width="70" data-sticky-height="60" data-sticky-top="33" src="img/logo_gb.png">
+									</a>
+								</div>
+							</div>
+							<div class="header-column">
+								<div class="header-row">
+								
+									
+								</div>
+								<div class="header-row">
+									<div class="header-nav">
+										<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main">
+											<i class="fa fa-bars"></i>
+										</button>
+										<!-- <ul class="header-social-icons social-icons hidden-xs">
+											<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+											<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+											<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+										</ul> -->
+										<div class="header-nav-main header-nav-main-effect-1 header-nav-main-sub-effect-1 collapse">
+											<nav>
+												<ul class="nav nav-pills" id="mainNav">
+													<li class="dropdown dropdown-mega">
+														<a href="index.php">Home</a>
+													</li>
+													<li class="dropdown dropdown-mega">
+														<a href="about_us.php">About Us</a>
+													</li>
+													<!-- <li class="dropdown dropdown-mega">
+														<a href="team.php">Team</a>
+													</li> -->
+													<li class="dropdown">
+														<a class="dropdown-toggle" href="#">Products</a>
+														<ul class="dropdown-menu">
+															<li><a href="#" >Sweatwater Fish</a></li>
+															<li><a href="#">Saltwater Fish</a></li>
+															<li><a href="#">Shrimp</a></li>
+														</ul>
+													</li>
+													<li class="dropdown">
+														<a class="dropdown-toggle" href="#">Sister Concerns</a>
+														<ul class="dropdown-menu">
+															<li><a href="#" >Company 1</a></li>
+															<li><a href="#">Company 2</a></li>
+															<li><a href="#">Company 3</a></li>
+														</ul>
+													</li>
+													<li class="dropdown dropdown-mega">
+														<a href="contact.php">Contact Us</a>
+													</li>
+													
+													<!-- <li class="dropdown">
+														<a class="dropdown-toggle" href="#">Login</a>
+														<ul class="dropdown-menu">
+															<li><a href="buyer/index.php" >Buyer</a></li>
+															<li><a href="sales/index.php">Seller</a></li>
+														</ul>
+													</li> -->
+												</ul>
+											</nav>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
