755ecfe777486676dbfc34263c2cf82449307b73
unifoxdigital <unifoxdigital.hihasan@gmail.com>
2018-11-08 06:24:12 +0600

    changes

---
+++ about_us.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<?php include('app.php'); ?>
+
+	</head>
+	<body data-spy="scroll" data-target="#sidebar" data-offset="120">
+
+		<div class="body">
+			<?php include ('header.php'); ?>
+
+			<div role="main" class="main">
+
+				<section class="page-header">
+					
+						
+						<iframe src="wowslider_about.html" style="width:100%;height:360px;max-width:100%;border:none;padding:0;margin:0 auto;display:block;" marginheight="0" marginwidth="0"></iframe>
+					
+				</section>
+
+				<div class="container">
+
+					<div class="row">
+						<div class="col-md-3">
+							<aside class="sidebar" id="sidebar" data-plugin-sticky >
+								<!--Side Image will apear here-->
+								
+								<img src="img/about_us.png" style="height: 760px; width: 160px;">
+						
+								
+								<!-- <h4 class="heading-primary">Categories</h4>
+								<ul class="nav nav-list mb-xl show-bg-active">
+									<li class="active"><a data-hash data-hash-offset="85" href="#first">First</a></li>
+									<li>
+										<a data-hash data-hash-offset="85" href="#second">Second</a>
+										<ul>
+											<li><a data-hash data-hash-offset="85" href="#sub-second-1">Sub Second 1</a></li>
+											<li><a data-hash data-hash-offset="85" href="#sub-second-2">Sub Second 2</a></li>
+										</ul>
+									</li>
+									<li><a data-hash data-hash-offset="85" href="#third">Third</a></li>
+								</ul>
+ -->
+							</aside>
+						</div>
+						<div class="col-md-9">
+
+							<h2 class="mb-sm" id="first" style="text-align: center;">About Us</h2>
+							<p>The unpolluted brackish waters of the Bay of Bengal are home of various species of marine shrimps and demersal fish. Among marine shrimps, major species in terms of volume of yield from catches, landings and aquaculture are the giant tiger shrimps and yellow shrimps.</p>
+
+							<!-- <img class="pull-left img-responsive" width="300" height="211" src="img/device.png" alt="Device"> -->
+
+							<p>From the inland fisheries resources, the major yield for international marketing remains the giant river fish and Cox’s Bazar Sea Foods, operating under the same management, market these products among others, in the international marketing chain for food, fish and sea foods, the former markets under Cox’s Bazar brand, US FDA Code 34.</p>
+
+							<p>The major areas of emphasis, strictly followed by the processing units, for quality control are as follows:</p>
+							<ol style="ol.f {list-style-type: decimal;}">
+								<li>Instant preservation on catch / capture / landing / harvest.</li>
+								<li>Reduced transportation time to factory on catch / capture / landing / harvest.</li>
+								<li>Improved quality of post-catch / harvest handling and transportation</li>
+								<li>Elimination of poor quality raw materials on arrival at factory and subsequent grading according to size, weight, etc.</li>
+								<li>Early packaging and processing of raw materials.</li>
+								
+							</ol>
+							<p>The plants and their facilities are routinely and regularly surveyed and approved by Lloyd’s surveyors. Products come under the routine pre-shipment inspection of the directorate of Fish Inspection & Quality Control Government of Bangladesh, reinforced by Lloyd’s contents, Salmonella, E. Coli, Coli Form, Weight, Grade, Appearance, Smell, etc.</p>
+
+							<p>The consistency in quality checks, tests and inspection from raw materials procurement through to export make our produces reliable in matters of health and hygiene and grades, sizes and standard. In marketing of such other exportable item, fresh water fish as well, appropriate and standard quality control procedures are rigorously followed.</p>
+
+							<p>In export shipments, our strength lies in our corporate capabilities in trading, enabling quick and efficient services, without failure of commitments.</p>
+
+							<p>The location of our factories in Cox’s Bazar, straddling the strategic points of raw materials production, procurement and export in Bangladesh, give us the leeway to serve the requirement of our clients better than the most.</p>
+
+							<p>Other than Cox’s Bazar, there are two more branch factories (Sister Concerns) as following:</p>
+
+							<ol>
+								<li>Name of the factory in Burma - A&Z International Co. Ltd.</li>
+								<li>Name of the factory in Oman - Mazoon Sky International L.L.C.</li>
+							</ol>
+
+							<p>CBSF exports their fish to China and USA. The fish from A&Z International Co. Ltd. are exported to Canada, Australia, Dubai, USA, UK, etc.</p>
+
+							<h2 class="mb-sm" id="first" style="text-align: center;">PACKING</h2><br>
+
+							<h3 class="mb-sm" id="second">Cox’s Bazar Sea Food (Cox’s Bazar Factory):</h3>
+
+							<p>Waxed corrugated white paper (3 ply) master cartons, containing 10 blocks of 2 KGs / 1.8 KGs / 4 LBs poly-wrapped and packed in colorfully printed waxed duplex board inner cartons. Variation in packing can be made to suit the requirements of buyer.</p>
+
+							<h3 class="mb-sm" id="second">Mazoon Sky International L.L.C. (Oman Factory):</h3>
+
+							<p>Waxed corrugated white paper (3 ply) master cartons, containing 10 KGs poly-wrapped and packed in colorfully printed waxed duplex board inner cartons. Variation in packing can be made to suit the requirements of buyer.</p>
+
+							<h3 class="mb-sm" id="second">A & Z International Co. Ltd. (Burma Factory):</h3>
+
+							<p>Waxed corrugated white paper (3 ply) master cartons, containing 10 blocks of 20KGs poly-wrapped and packed in colorfully printed waxed duplex board inner cartons. Variation in packing can be made to suit the requirements of buyer.</p>
+
+
+							<!-- h2 class="mb-sm" id="second">Second</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p> -->
+
+							<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+
+							<h4 class="mb-sm" id="sub-second-1">Sub Second 1</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+
+							<h4 class="mb-sm" id="sub-second-2">Sub Second 2</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+
+							<h2 class="mb-sm" id="third">Third</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p> -->
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<?php include ('footer.php'); ?>
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.js"></script>
+		<script src="vendor/jquery-cookie/jquery-cookie.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.js"></script>
+		<script src="vendor/common/common.js"></script>
+		<script src="vendor/jquery.validation/jquery.validation.js"></script>
+		<script src="vendor/jquery.stellar/jquery.stellar.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.js"></script>
+		<script src="vendor/jquery.lazyload/jquery.lazyload.js"></script>
+		<script src="vendor/isotope/jquery.isotope.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+		<script src="vendor/vide/vide.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+			ga('create', 'UA-12345678-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+		 -->
+
+	</body>
+</html>
