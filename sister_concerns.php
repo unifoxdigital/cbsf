d82e6c6c4ef37bd61e30ff520b7bd4254aca7f5a
hihasan <hasan@hihsan.com>
2019-01-23 12:17:29 +0600

    sister concerns page done

--- sister_concerns.php
+++ sister_concerns.php
@@ -45,16 +45,32 @@
 
 						<div class="row" style="margin-bottom: 16px;">
 					        <div class="col-lg-4 mb-4"> 
+					        	<img src="img/sister_concerns/1.jpg" style="width: 348px; height: 258px;">
+					        	<h3 style="color: #fff; margin-top: 8px;">Cox's Bazar Seafood</h3>
+					        	<h4 style="color: #fff;">Dhaka and Cox's Bazar, Bangladesh.</h4>
+					        	<p style="color: #fff;">Cox's Bazar Seafood (CBSF) was established in 1986 and strategically located itself in the seaside city of Cox’s Bazar, Bangladesh, with the goal to source and export the highest quality and sustainable frozen seafood and freshwater fish products. Using its state of the art processing plant, freezers, and packaging machines, CBSF has successfully maintained consistent quality and supplies seafood and freshwater fish to clients around the world.</p>
+					        	<p style="color: #fff;">Address:House# 413, Rd# 7 (West), Baridhara DOHS, Dhaka, Bangladesh.</p>
 					        </div>
 
 					        
 					        <div class="col-lg-4 mb-4">
+					        	<img src="img/sister_concerns/2.jpg" style="width: 348px; height: 258px;">
+					        	<h3 style="color: #fff; margin-top: 8px;">A&Z International Co. Ltd. & Global Seafood Processing Limited</h3>
+					        	<h4 style="color: #fff;">Yangon, Myanmar.</h4>
+					        	<p style="color: #fff;">The pursuit of quality and variety led CBSF to expand to its neighbouring country Myanmar in 1991. A&Z International Co. Ltd. (A&Z) and Global Seafood Processing (GSP) ware established and a new state of the art processing factory, including freezing and packaging, was built. Using this strategic location, A&Z and GSP continue to purchase, freeze, package, and export the highest quality of sustainable seafood and freshwater fish.</p>
+
+					        	<p style="color: #fff;">Address:No - 46/B, New Excellent Condo, Room# 4 (C), Pantra Street, Paya Gyi Ward, Dangon Township, Yangon, Myanmar.</p>
 					        </div>
 					        
 
 					        <div class="col-lg-4 mb-4">
-					      </div>
-					</div>
+					        	<img src="img/sister_concerns/3.jpg" style="width: 348px; height: 258px;">
+					        	<h3 style="color: #fff; margin-top: 8px;">Mazoon Sky International L.L.C.</h3>
+					        	<h4 style="color: #fff;">Muscat, Oman.</h4>
+					        	<p style="color: #fff;">The increasing reputation for consistent  quality seafood and freshwater fish, and respecting the client's urgency helped the group to grow more. Mazoon Sky International (MSI) LLC was strategically established in Muscat, Oman's seaport capital, in 2016, to meet the increasing demand for seafood. MSI continues to source and export premium quality sustainable seafood to clients around the world.</p>
+					        	<p style="color: #fff;">Address: C.R. No. 1294513, P.O. Box: 91, P.C.: 114, Sultanate of Oman.​</p>
+					      	</div>
+						</div>
 					</div>
 
 				</div>
